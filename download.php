6ebc17901643d9ab0ed8e4b30f2e35c5d16cb080
wouter van der plas <wouterv.dplas@gmail.com>
2016-02-23 21:26:39 +0100

    fixed a bug: it didnt download if id stated with a '-'

--- download.php
+++ download.php
@@ -2,6 +2,8 @@
 	include("urlAsambler.php");
 	$type = str_replace("yt:", "", $_GET["type"]);
 	$id = $_GET["id"];
+	
+	$url = "https://www.youtube.com/watch?v=".$id;
 
 	if(file_exists("/var/www/html/youtube-rss/cash/".$id.".mp3")){
 		header("location: http://cwms.cc/youtube-rss/cash/".$id.".mp3");
@@ -10,7 +12,7 @@
 
 		//echo 'youtube-dl -x --audio-format mp3 -o "/var/www/html/youtube-rss/cash/%(id)s.%(ext)s" '.$id;
 
-		$output = exec('youtube-dl -x --audio-format mp3 -o "/var/www/html/youtube-rss/cash/%(id)s.%(ext)s" '.$id, $ret);
+		$output = exec('youtube-dl -x --audio-format mp3 -o "/var/www/html/youtube-rss/cash/%(id)s.%(ext)s" "'.$url.'"', $ret);
 		header("location: http://cwms.cc/youtube-rss/cash/".$id.".mp3");
 	}
 ?>
