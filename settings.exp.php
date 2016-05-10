4617ce313f148a84494d2f4d90c41caa7a9627cd
wouter van der plas <wouterv.dplas@gmail.com>
2016-05-10 14:38:26 -0400

    some last debugging

--- settings.exp.php
+++ settings.exp.php
@@ -4,7 +4,7 @@ $youtube_dl_bin = "youtube-dl";
 
 $domain = "yourdomain";
 $path = "youtube-rss/";
-$root_dir = "/var/www/html/youtube-rss";
+$root_dir = "/var/www/html/youtube-rss/";
 $cash_location = "cash/";
 
 $episodePerRss = 20;
--- status.php
+++ status.php
@@ -4,6 +4,7 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
     echo '<h1>This feature is linux only!</h1>';
     die();
 }
+
 define("br", "<br>");
 require "settings.php";
 
@@ -11,15 +12,15 @@ echo "<h2>hard disk drive space</h2>";
 
 $bin = "df";
 $output = shell_exec($bin);
-var_dump($output);
+echo nl2br($output);
 echo br;
 
 
 echo "<h2>size of the cash folder</h2>";
 
-$bin = "du -sh ".$root_dir.$cash_location;;
+$bin = "du -sh ".$root_dir.$cash_location;
 $output = shell_exec($bin);
-var_dump($output);
+echo($output);
 echo br;
 
 echo "<h2>test env</h2>";
